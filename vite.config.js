ef15c83182b1f2f7689aade244811329d7eb2215
Aryan Bhardwaj <aryanbhardwaj5830@gmail.com>
2026-05-06 16:07:19 +0530

    Simplified vite config paths

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        account: resolve(__dirname, 'account.html'),
-        article: resolve(__dirname, 'article.html'),
-        auth: resolve(__dirname, 'auth.html'),
-        blog: resolve(__dirname, 'blog.html'),
-        cart: resolve(__dirname, 'cart.html'),
-        events: resolve(__dirname, 'events.html'),
-        product: resolve(__dirname, 'product.html'),
-        shop: resolve(__dirname, 'shop.html'),
-        support: resolve(__dirname, 'support.html'),
-        wishlist: resolve(__dirname, 'wishlist.html'),
-      },
-    },
-  },
-});
+        main: 'index.html',
+        about: 'about.html',
+        account: 'account.html',
+        article: 'article.html',
+        auth: 'auth.html',
+        blog: 'blog.html',
+        cart: 'cart.html',
+        events: 'events.html',
+        product: 'product.html',
+        shop: 'shop.html',
+        support: 'support.html',
+        wishlist: 'wishlist.html',
+      }
+    }
+  }
+})
+
